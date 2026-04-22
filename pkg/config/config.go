bb5682fa101ae2aff89167f7976634999ded14df
Gorkyi Chocolate <beybarskrutoi572@gmail.com>
2026-04-22 23:10:32 +0500

    1231

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	FromEmail         string
+	FromEmailPassword string
+	FromEmailSMTP     string
+	SMTPAddr          string
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading .env file: %v", err)
+		return nil, err
+	}
+
+	config := &Config{
+		FromEmail:         os.Getenv("FROM_EMAIL"),
+		FromEmailPassword: os.Getenv("FROM_EMAIL_PASSWORD"),
+		FromEmailSMTP:     os.Getenv("FROM_EMAIL_SMTP"),
+		SMTPAddr:          os.Getenv("SMTP_ADDR"),
+	}
+
+	return config, nil
+}
---
+++ pkg/go.mod
@@ -0,0 +1,5 @@
+module smart-parking/pkg
+
+go 1.22.2
+
+require github.com/joho/godotenv v1.5.1
