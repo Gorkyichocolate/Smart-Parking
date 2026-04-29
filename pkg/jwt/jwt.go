938242137c51f4cf87b920928d9c45784e57f26b
Dulat Amangeldy <dulatamangeldy@MacBook-Air-Dulat-3.local>
2026-04-29 17:52:21 +0500

    обновил jwt

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,1 +1,20 @@
 package jwt
+
+import (
+	"os"
+	"time"
+)
+
+var (
+	SecretKey  = []byte(getEnv("JWT_SECRET", "smart-parking-secret-key-2024"))
+	AccessTTL  = 15 * time.Minute
+	RefreshTTL = 168 * time.Hour
+	IssuerName = "smart-parking"
+)
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
